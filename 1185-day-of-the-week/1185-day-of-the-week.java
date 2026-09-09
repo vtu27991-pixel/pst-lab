6c9c463a91c43c1085e1ad46225d86784b7a4d76
vtu27991-pixel <vtu27991@veltech.edu.in>
2026-09-09 10:16:16 +0530

    Time: 0 ms (100%), Space: 42.7 MB (24.44%) - LeetHub

---
+++ 1185-day-of-the-week/1185-day-of-the-week.java
@@ -0,0 +1,51 @@
+class Solution {
+    public String dayOfTheWeek(int day, int month, int year) {
+
+        String[] days = {
+            "Sunday",
+            "Monday",
+            "Tuesday",
+            "Wednesday",
+            "Thursday",
+            "Friday",
+            "Saturday"
+        };
+
+        // Days in each month
+        int[] monthDays = {
+            31, 28, 31, 30, 31, 30,
+            31, 31, 30, 31, 30, 31
+        };
+
+        int totalDays = 0;
+
+        // Count days from 1971 to the given year
+        for (int y = 1971; y < year; y++) {
+            if (isLeapYear(y)) {
+                totalDays += 366;
+            } else {
+                totalDays += 365;
+            }
+        }
+
+        // Count days from January to the previous month
+        for (int m = 1; m < month; m++) {
+            totalDays += monthDays[m - 1];
+
+            if (m == 2 && isLeapYear(year)) {
+                totalDays++;
+            }
+        }
+
+        // Add days before the given date
+        totalDays += day - 1;
+
+        // January 1, 1971 was Friday (index 5)
+        return days[(totalDays + 5) % 7];
+    }
+
+    private boolean isLeapYear(int year) {
+        return (year % 400 == 0) ||
+               (year % 4 == 0 && year % 100 != 0);
+    }
+}
